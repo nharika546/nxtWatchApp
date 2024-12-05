50876cfc913310ba7839f18694bf74d4a9517739
harika <sriharikanuka@gmail.com>
2024-12-05 13:26:12 +0000

    solution

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ThemeContext = React.createContext({
+  isDarkTheme: false,
+  toggleTheme: () => {},
+  savedVideosList: [],
+  savedVideoButtonClicked: () => {},
+})
+
+export default ThemeContext
